f89b484940d6618db4e65fc25cc80bc13329258b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-07-18 19:08:41 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -15,8 +15,8 @@ const LaunchRequestHandler = {
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
         
         // const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
 
