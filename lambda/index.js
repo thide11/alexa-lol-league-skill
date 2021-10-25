f5e9c91e8b6eafbce9f3a81b8a0e2ac8de5168e7
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-10-25 19:06:54 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -29,7 +29,7 @@ const LaunchRequestHandler = {
         
         if(!accessToken) {
             return handlerInput.responseBuilder
-              .speak("Por favor, vincule sua conta para cadastrar seu nick no lol!")
+              .speak("Por favor, vincule sua conta para cadastrar seu nick do lol!")
               .withLinkAccountCard()
               .getResponse();
         }
@@ -38,7 +38,7 @@ const LaunchRequestHandler = {
         const rankedSolo = (await axios.get(`${baseUrl}/getElo?jwt=${accessToken}`)).data
         if(rankedSolo.message) {
             return handlerInput.responseBuilder
-              .speak("Cadastro de nickname do lol necessário, verifique o card adicionado no seu app")
+              .speak("Cadastro de nickname do lol necessário, verifique o card adicionado no seu app da alexa")
               .withSimpleCard(
                 "Vinculação de nick necessária",
                 `Por favor, Acesse ${baseUrl} pelo navegador, autentique com sua conta amazon e insire seu nome de invocador lá`
@@ -46,149 +46,8 @@ const LaunchRequestHandler = {
               .getResponse();
         }
         const eloMessage = `Você está no ${eloToPortuguese[rankedSolo.tier]} ${rankedSolo.rank}, com ${rankedSolo.leaguePoints} de PDL`;
-
-        const pictureUrl = Util.getS3PreSignedUrl("Media/gemidao.mp3").replace(/&/g,'&amp;');
-        const speakOutput = `<audio src="${pictureUrl}"/> <say-as interpret-as="interjection">calma</say-as>. Você foi <emphasis level="strong">trolado</emphasis>`;
-        const anotherSpeakOutput = `
-    Poema do cume
-    
-    No alto daquele cume.
-    Plantei uma roseira.
-    O vento no cume bate.
-    A rosa no cume cheira.
-    
-    Quando vem a chuva fina.
-    Salpicos no cume caem.
-    Formigas no cume entram.
-    Abelhas do cume saem.
-    
-    Quando cai a chuva grossa.
-    A água do cume desce.
-    O barro do cume escorre.
-    O mato no cume cresce.
-    
-    Então, quando cessa a chuva.
-    No cume volta a alegria.
-    Pois torna a brilhar de novo.
-    O Sol que no cume ardia.`;
-    const speaker3 = "A vai ti toma no cu rapa"
-    
-        // const eloMessage = await fetchMyEloToMessage();
-
         return handlerInput.responseBuilder
             .speak(eloMessage)
-            .getResponse();
-    }
-};
-
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
-
-const InvestimentoIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'Investimento';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Sua carteira está com 1120,30';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse();
-    }
-};
-
-const HelpIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
-
-const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Goodbye!';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse();
-    }
-};
-/* *
- * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
- * It must also be defined in the language model (if the locale supports it)
- * This handler can be safely added but will be ingnored in locales that do not support it yet 
- * */
-const FallbackIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
-/* *
- * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
- * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
- * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
- * */
-const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
-    }
-};
-/* *
- * The intent reflector is used for interaction model testing and debugging.
- * It will simply repeat the intent the user said. You can create custom handlers for your intents 
- * by defining them above, then also adding them to the request handler chain below 
- * */
-const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
-    },
-    handle(handlerInput) {
-        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = `You just triggered ${intentName}`;
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
@@ -219,14 +78,7 @@ const ErrorHandler = {
  * */
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        InvestimentoIntentHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        FallbackIntentHandler,
-        SessionEndedRequestHandler,
-        IntentReflectorHandler)
+        LaunchRequestHandler,)
     .addErrorHandlers(
         ErrorHandler)
     .withCustomUserAgent('sample/hello-world/v1.2')
