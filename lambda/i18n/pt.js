34a5ec3ad4e7fbccc332814cafd83a15f0cf59c8
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-10-25 20:43:34 +0000

    updated lambda/i18n/pt.js.

---
+++ lambda/i18n/pt.js
@@ -0,0 +1,19 @@
+module.exports = {
+    translation: {
+        'NEED_VINCULATION': "Por favor, vincule sua conta para cadastrar seu nick do lol!",
+        "NEED_NICKNAME_HEADER": "Cadastro de nickname do lol necessário, verifique o card adicionado no seu app da alexa",
+        "NEED_NICKNAME_CARD_HEADER": "Vinculação de nick necessária",
+        "NEED_NICKNAME_CARD_DESCRIPTION": "Por favor, Acesse %s pelo navegador, autentique com sua conta amazon e insire seu nome de invocador lá",
+        "ELO_TEMPLATE": "Você está no %s %s, com %s de PDL",
+        "UNRANKED_MESSAGE": "Você não tem elo na solo/duo",
+        "IRON": "FERRO",
+        "BRONZE": "BRONZE",
+        "SILVER": "PRATA",
+        "GOLD": "OURO",
+        "PLATINUM": "PLATINA",
+        "DIAMOND": "DIAMANTE",
+        "MASTER": "MESTRE",
+        "GRANDMASTER": "GRÃO MESTRE",
+        "CHALLANGER": "DESAFIANTE",
+    }
+}
