453d7f49a19c585f34228e82cae605d5ae3c30f4
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-15 16:46:52 +0000

    updated lambda/i18n/pt.js.

--- lambda/i18n/pt.js
+++ lambda/i18n/pt.js
@@ -3,7 +3,7 @@ module.exports = {
         'NEED_VINCULATION': "Para começar a usar esta skill, visite a seção inicial do aplicativo Alexa e clique no link para se conectar com o Alexa",
         "NEED_REGISTER_NICKNAME": "Cadastro de nickname do lol necessário, acesse o site %s pelo navegador, logue com a amazon e insira seu nick",
         "ELO_TEMPLATE": "Você está no %s %s, com %s de PDL",
-        "UNRANKED_MESSAGE": "Você não tem elo na solo/duo",
+        "UNRANKED_MESSAGE": "Você não tem elo na ranqueda solo/duo",
         "PROBLEM_MESSAGE": "Desculpe, estou com problema para processar o que você falou, por favor tente novamente mais tarde",
         "IRON": "FERRO",
         "BRONZE": "BRONZE",
