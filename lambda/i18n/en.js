395b79d02c5a72ad331249c83da07c5e3dbbcbe7
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-15 16:43:31 +0000

    updated lambda/i18n/en.js.

--- lambda/i18n/en.js
+++ lambda/i18n/en.js
@@ -1,8 +1,7 @@
 module.exports = {
     translation: {
         'NEED_VINCULATION': "To start using this skill, please visit the home section of the Alexa app and click on the link to connect with the Alexa",
-        "NEED_NICKNAME_HEADER": "League of legends Nickname registration required, check the card added in your alexa app",
-        "NEED_NICKNAME_CARD_HEADER": "Nick binding required",
+        "NEED_REGISTER_NICKNAME": "League of legends nickname registration required, acess the site %s with an browser, sign in with amazon and place your nickname",
         "NEED_NICKNAME_CARD_DESCRIPTION": "Please login to %s via browser, authenticate with your amazon account and enter your summoner name there",
         "ELO_TEMPLATE": "You are on %s %s, with %s of LP",
         "UNRANKED_MESSAGE": "You have no link in solo/duo",
