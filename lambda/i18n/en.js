4de1f77d860f354d5ccd7b91d73e5edea1d5e9e1
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-15 16:45:38 +0000

    updated lambda/i18n/en.js.

--- lambda/i18n/en.js
+++ lambda/i18n/en.js
@@ -4,7 +4,7 @@ module.exports = {
         "NEED_REGISTER_NICKNAME": "League of legends nickname registration required, acess the site %s with an browser, sign in with amazon and place your nickname",
         "ELO_TEMPLATE": "You are on %s %s, with %s of LP",
         "UNRANKED_MESSAGE": "You have no rank in ranked solo/duo",
-        "PROBLEM_MESSAGE": "Sorry, I had trouble doing what you asked. Please try again.",
+        "PROBLEM_MESSAGE": "Sorry, I had trouble doing what you asked. Please try again later.",
         "IRON": "IRON",
         "BRONZE": "BRONZE",
         "SILVER": "SILVER",
