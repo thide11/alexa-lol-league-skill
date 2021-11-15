f65e7efad9cd90c2b5296b90805d7951defd8f50
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-11-15 16:45:29 +0000

    updated lambda/i18n/en.js.

--- lambda/i18n/en.js
+++ lambda/i18n/en.js
@@ -3,7 +3,7 @@ module.exports = {
         'NEED_VINCULATION': "To start using this skill, please visit the home section of the Alexa app and click on the link to connect with the Alexa",
         "NEED_REGISTER_NICKNAME": "League of legends nickname registration required, acess the site %s with an browser, sign in with amazon and place your nickname",
         "ELO_TEMPLATE": "You are on %s %s, with %s of LP",
-        "UNRANKED_MESSAGE": "You have no rank in solo/duo",
+        "UNRANKED_MESSAGE": "You have no rank in ranked solo/duo",
         "PROBLEM_MESSAGE": "Sorry, I had trouble doing what you asked. Please try again.",
         "IRON": "IRON",
         "BRONZE": "BRONZE",
